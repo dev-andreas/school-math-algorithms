322a8c9ac6036731c0098d30e713cc9a749924fc
Andreas Gerasimow <andreas.gerasimow12@gmail.com>
2021-02-11 15:34:03 +0100

    - expected value - standard deviation

--- stochastics/factorial.py
+++
@@ -1,7 +0,0 @@
-
-def fac(x: int) -> int:
-    ''' Calculates n-factorial (n!) '''
-    y = 1
-    for z in range(1,x):
-        y *= z+1
-    return y
--- stochastics/stochastics.py
+++ stochastics/stochastics.py
@@ -1,4 +1,10 @@
-from factorial import *
+def factorial(x: int) -> int:
+    ''' Calculates n-factorial (n!) '''
+    y = 1
+    for z in range(1,x):
+        y *= z+1
+    return y
+
 
 def bincoef(n: int, k: int) -> int:
     ''' Binomial coefficient function "n choose k" '''
@@ -14,7 +20,7 @@ def pmf(k: int, n: int, p: float) -> float:
 
 def map_pmf(k: tuple, n: int, p: float) -> tuple:
     '''
-    Cumulative distribution function. (Gives the propability that a discrete random variable is not greater than some value)
+    Probability mass function. (Gives the propability that a discrete random variable is exactly equal to some value)
     Takes tuple as first argument
     '''
     pmf_function = lambda x: pmf(x, n, p)
@@ -35,3 +41,18 @@ def map_cdf(k: tuple, n: int, p: float) -> tuple:
     '''
     cdf_function = lambda x: cdf(x, n, p)
     return tuple(map(cdf_function, k))
+
+def expected_value(values: tuple) -> float:
+    '''
+    Calculates the expected value of a values tuple e.g. ((value 1, probability 1), ..., (value n, probability n))
+    '''
+    function = lambda value: value[0] * value[1]
+    return sum(map(function, values))
+
+def standard_deviation(values: tuple) -> float:
+    '''
+    Calculates the standard deviation of a values tuple e.g. ((value 1, probability 1), ..., (value n, probability n))
+    '''
+    my = expected_value(values)
+    sigma_function = lambda value: (value[0] - my)**2 * value[1]
+    return sum(map(sigma_function, values))**(1/2)
